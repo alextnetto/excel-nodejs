c649f05ef2e686fce0e492335b56da12599aa2ec
Alexandro T. Netto <alex.t.netto@gmail.com>
2021-04-14 00:07:27 -0300

    Bad convertion xlsx to html

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ const morgan = require('morgan')
 const { data } = require('./data')
 const generatePdfHtml = require('./utils/pdfHtml')
 const Excel = require('exceljs')
+const XLSX = require('xlsx')
 
 const app = express()
 
@@ -22,77 +23,39 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname +  '/index.html')
 })
 
-
-app.post('/excel/upload', async (req, res) => {
-    console.log(req.files)
-
-    if (req.files.file) {
-        file = req.files.file
-        const tmpPath = path.resolve(__dirname, '..', 'tmp', 'uploads') + '/'
-
-
-        allowedMimeTypes = [
-            'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
-        ]
-
-        if (file.size > 2 * 1024 * 1024) {
-            return res.status(413).send({'error': 'File must be less than 10MB'})     
-        }
-        if (!allowedMimeTypes.includes(file.mimetype)) {
-            return res.status(415).send({'error': 'File type not suported'})
-        }
-
-        //Save file in tmp folder
-        file.mv(tmpPath + file.name)
-
-        //Parse excel buffer to object
-        var xlsxData = xlsx.parse(file.data)[0].data
-
-        // TODO: Get user data from DB with id
-        userData = data[req.body.id]
-        finalResponse = {}
-
-        newXlsxData = xlsxData.map((row) => {
-            return row.map((cell) => {
-                cell = String(cell)
-                if (cell.startsWith('<%') && cell.endsWith('%>')) {
-                    cellTag = cell.substring(2, cell.length - 2)
-                    cellValue = userData[cellTag]
-                    finalResponse[cellTag] = cellValue
-                    return cellValue
-                }
-                return cell
-            })
-        })
-
-        generatePdfHtml(res, finalResponse)
-        // pdfBuffer = await generatePdfHtml()
-        // return res.end(pdfBuffer)
-        // return res.download(tmpPath + 'Finalizado - 4642.pdf')
-    } else {
-        return res.status(406).send({'error': 'No files'})
-    }
-})
-
 app.post('/exceljs', async (req, res) => {
     const serviceId = req.body.id
     if (serviceId === undefined) {
         return res.status(400).json({'error': 'Service ID is required'})
     }
+    const file = req.files.file
+    if (file === null) {
+        return res.status(406).json({'error': 'No files'})
+    }
+
+
+    allowedMimeTypes = [
+        'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+    ]
+    if (!allowedMimeTypes.includes(file.mimetype)) {
+        return res.status(415).json({'error': 'File must be .xlsx'})
+    }
+    
+    if (file.size > 10 * 1024 * 1024) {
+        return res.status(413).json({'error': 'File must have less than 10MB'})     
+    }
 
     function getCellValueFromDB(cell, serviceId) {
         tag = cell.value
         tag = tag.substring(2, tag.length - 2)
-        console.log(data[serviceId], tag)
         value = data[serviceId][tag]
-        console.log(value === undefined ? cell.value : value)
         return value === undefined ? cell.value : value
     }
     
     const tmpPath = path.resolve(__dirname, '..', 'tmp', 'uploads') + '/'
 
     const workbook = new Excel.Workbook();
-    await workbook.xlsx.load(req.files.file.data);
+    await workbook.xlsx.load(file.data);
     data1 = []
     const worksheet = workbook.getWorksheet(1)
 
@@ -122,6 +85,14 @@ app.post('/exceljs', async (req, res) => {
     });
     res.end(buffer) 
 
+    wb = XLSX.read(buffer)
+    ws = wb.Sheets['Table 1']
+    fs = require('fs');
+    html = await XLSX.utils.sheet_to_html(ws)
+    fs.writeFile('./teste.html', html, function (err) {
+        if (err) return console.log(err);
+        console.log('Hello World > helloworld.txt');
+      });
 })
 
 app.listen(PORT, (req, res) => {
